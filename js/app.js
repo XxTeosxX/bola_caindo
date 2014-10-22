be869ccfb99feb84596f253b4c8ce3d36c889ea7
Gleison Rodrigues <xgleisonx@gmail.com>
2014-10-21 21:17:05 -0300

    bola no topo

--- js/app.js
+++ js/app.js
@@ -1,37 +1,56 @@
-var barraAltura, barraLargura, jogadorPosicaoX, velocidadeJogador;
+var barraAltura,
+    barraLargura,
+    jogadorPosicaoX,
+    velocidadeJogador,
+    bolaDiametro,
+    bolaPosX,
+    bolaPosY,
+    velocidadeBola;
+
 function inicializar(){
-  velocidadeJogador = 20;
+  bolaDiamentro = 10;
+  bolaPosX = canvas.width / 2;
+  bolaPosY = 0;
+  velocidadeBola = 10;
+
   barraAltura = 15;
   barraLargura = 90;
 
-  
+
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
 
   jogadorPosicaoX = (canvas.width - barraLargura)/2;
+  velocidadeJogador = 20;
 
   context.fillRect(
-          jogadorPosicaoX, 
-          canvas.height - barraAltura, 
-          barraLargura, 
+          jogadorPosicaoX,
+          canvas.height - barraAltura,
+          barraLargura,
           barraAltura
   );
 
   document.addEventListener('keydown', keyDown);
+
+  setInterval(gameLoop, 30);
 }
 
 function keyDown(e){
-  if(e.keyCode == 37){
+  if(e.keyCode == 37 && jogadorPosicaoX > 0){
     jogadorPosicaoX -= velocidadeJogador;
-    console.log(jogadorPosicaoX);
-  }else if(e.keyCode == 39){
+  }else if(e.keyCode == 39 && jogadorPosicaoX < (canvas.width - barraLargura)){
     jogadorPosicaoX += velocidadeJogador;
   }
   context.fillRect(
-          jogadorPosicaoX, 
-          canvas.height - barraAltura, 
-          barraLargura, 
+          jogadorPosicaoX,
+          canvas.height - barraAltura,
+          barraLargura,
           barraAltura
   );
-  
+
+}
+
+function gameLoop(){
+  context.clearRect(0,0, canvas.width, canvas.height);
+  context.fillRect(jogadorPosicaoX, canvas.height - barraAltura, barraLargura, barraAltura);
 }
